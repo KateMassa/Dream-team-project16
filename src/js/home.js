74090434e8cec2a89aa43f4ea04a1f5756045bee
KateMassa <catherineorlova27@gmail.com>
2024-02-27 18:29:32 +0100

    upd

--- src/js/home.js
+++
@@ -1,3 +0,0 @@
-import { renderBestsellers } from './bestsellers.js';
-
-renderBestsellers();
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// === Відображення блоку книг бестселлерів
+import { renderBestsellers } from './js/bestsellers';
+renderBestsellers();
+
+// === Відображення книг за каталогом
+import { renderCategory } from './js/renderCategory';
+
+renderCategory('Advice How-To and Miscellaneous');
